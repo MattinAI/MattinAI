996336a4752569d9fd739bde771abd23e8b65953
anoya <anoya@ikerlan.es>
2025-03-31 12:39:07 +0200

    refactor: langflow

---
+++ langflow/langflow/src/backend/base/langflow/base/anonymization/presidio_constants.py
@@ -0,0 +1,15 @@
+NER_MODEL_NAME = [
+    "spaCy", 
+    "Flair", 
+    "HuggingFace", 
+    "stanza", 
+    "Azure AI Language"
+],
+ANONYMIZATION_METHOD = [
+    "replace", 
+    "redact", 
+    "mask", 
+    "hash", 
+    "encrypt", 
+    "highlight"
+]
